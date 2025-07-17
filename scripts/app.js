f4a780de4e3acdf80c74cb3b6ee16cd6721647f1
itssadiq <sadiqalishaikh009@gmail.com>
2025-07-17 11:18:58 +0500

    - Fetched data successfully and saved in local storage.

--- scripts/app.js
+++ scripts/app.js
@@ -1,30 +1,23 @@
-// const link = "https://opentdb.com/api_config.php";
+async function fetchData(categoryValue, difficultyValue) {
+  const URL = `https://opentdb.com/api.php?amount=10&category=${categoryValue}&difficulty=${difficultyValue}&type=multiple`;
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    const questions = data.results;
 
-// const generalKnowledge = "9";
-
-// const computerScience = "18";
-
-// const history = "23";
-
-// const sports = "21";
-
-// async function fetchData() {
-//   const URL = `https://opentdb.com/api.php?amount=10&category=${sports}&difficulty=medium&type=multiple`;
-//   try {
-//     const response = await fetch(URL);
-//     const data = await response.json();
-//     console.log(data.results);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-// fetchData();
+    localStorage.setItem("questions", JSON.stringify(questions));
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 const categorySelectorEl = document.getElementById("category");
 const difficultySelectorEl = document.getElementById("difficulty");
 const startEl = document.querySelector("button");
 const activeBtn = document.querySelector(".start-active");
+
+categorySelectorEl.addEventListener("change", makeButtonActive);
+difficultySelectorEl.addEventListener("change", makeButtonActive);
 
 function makeButtonActive() {
   const categoryValue = categorySelectorEl.value;
@@ -33,6 +26,7 @@ function makeButtonActive() {
   if (categoryValue && difficultyValue) {
     startEl.classList.remove("start");
     startEl.classList.add("start-active");
+    fetchData(categoryValue, difficultyValue);
     startEl.addEventListener("click", operation);
   } else {
     startEl.classList.remove("start-active");
@@ -44,6 +38,3 @@ function makeButtonActive() {
 function operation() {
   window.location.href = "quiz.html";
 }
-
-categorySelectorEl.addEventListener("change", makeButtonActive);
-difficultySelectorEl.addEventListener("change", makeButtonActive);
